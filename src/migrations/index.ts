21d8f3ca761e73efb8affad2e12071ee75310684
Markus Sommer <markus@letsbenow.de>
2026-02-13 08:30:15 +0100

    feat: remove obsolete migration file and update migration index

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260212_155102 from './20260212_155102';
-
-export const migrations = [
-  {
-    up: migration_20260212_155102.up,
-    down: migration_20260212_155102.down,
-    name: '20260212_155102'
-  },
-];
